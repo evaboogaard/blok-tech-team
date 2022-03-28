52af99481d8cfc309be99b996d4c518765f2b60d
Eva Boogaard <evaboogaard@hotmail.com>
2022-03-28 13:38:25 +0200

    :sparkles: added some clientside js for the nav

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,1 +1,23 @@
 console.log("waddup");
+
+const homeButton = document.querySelector("nav img:nth-of-type(1)");
+const likeButton = document.querySelector("nav img:nth-of-type(2)");
+const accountButton = document.querySelector("nav img:nth-of-type(3)");
+
+homeButton.addEventListener("click", () => {
+    homeButton.classList.add("active");
+    likeButton.classList.remove("active");
+    accountButton.classList.remove("active");
+})
+
+likeButton.addEventListener("click", () => {
+    homeButton.classList.remove("active");
+    likeButton.classList.add("active");
+    accountButton.classList.remove("active");
+})
+
+accountButton.addEventListener("click", () => {
+    homeButton.classList.remove("active");
+    likeButton.classList.remove("active");
+    accountButton.classList.add("active");
+})
