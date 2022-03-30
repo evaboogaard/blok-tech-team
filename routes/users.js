e07634c620bc731e6ba055b62d6cbf4674830d1a
Eva Boogaard <evaboogaard@hotmail.com>
2022-03-30 13:15:08 +0200

    :lipstick: header styling

--- routes/users.js
+++ routes/users.js
@@ -8,84 +8,81 @@ const saltRounds = 10;
 
 const nodemailer = require("nodemailer");
 
-router.use(bodyParser.urlencoded({extended: true}));
-
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.post("/createaccount", async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-    const user = new User({
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        password: hashedPassword
-    });
+  const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+  const user = new User({
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    password: hashedPassword,
+  });
 
-    user.save((error) => {
-        if (error) {
-            console.error(error);
-            return res.status(500).redirect('createaccount');
-        } else {
-            console.log("Account aangemaakt!")
+  user.save((error) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).redirect("createaccount");
+    } else {
+      console.log("Account aangemaakt!");
 
-            let transporter = nodemailer.createTransport({
-                service: "hotmail",
-                auth: {
-                  user: "dinder.co@hotmail.com",
-                  pass: "dinder420",
-                },
-              });
-            
-              transporter.sendMail({
-                from: '"Dinder" <dinder.co@hotmail.com>', // sender
-                to: user.email, // receiver
-                subject: "Welcome to Dinder🍽!", // subject
-                text: "Hi " + user.fname + " " + user.lname + ", welcome to Dinder!", // body
-              });
+      let transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+          user: "dinder.co@hotmail.com",
+          pass: "dinder420",
+        },
+      });
 
-            return res.render("overviewaccount", {
-                fname: user.fname,
-                lname: user.lname,
-                email: user.email
-            });
-            
-        }
-    });
-});
+      transporter.sendMail({
+        from: '"Dinder" <dinder.co@hotmail.com>', // sender
+        to: user.email, // receiver
+        subject: "Welcome to Dinder🍽!", // subject
+        text: "Hi " + user.fname + " " + user.lname + ", welcome to Dinder!", // body
+      });
 
-
-
-router.post('/login', async (req, res) => {
-    try {
-        const getUser = await User.findOne({ email: req.body.email });
-        if (getUser) {
-          const comparePassword = await bcrypt.compare(req.body.password, getUser.password);
-          if (comparePassword) {
-            console.log("It's a great success!");
-            return res.status(200).redirect('/overviewaccount');
-          } else {
-            console.error("Wrong e-mail or password!");
-            return res.status(404).redirect('/login');
-          }
-        } else {
-            console.error("Wrong e-mail or password!!");
-            return res.status(404).redirect('/login');
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).redirect('/login');
+      return res.render("overviewaccount", {
+        fname: user.fname,
+        lname: user.lname,
+        email: user.email,
+      });
     }
+  });
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const getUser = await User.findOne({ email: req.body.email });
+    if (getUser) {
+      const comparePassword = await bcrypt.compare(
+        req.body.password,
+        getUser.password
+      );
+      if (comparePassword) {
+        console.log("It's a great success!");
+        return res.status(200).redirect("/overviewaccount");
+      } else {
+        console.error("Wrong e-mail or password!");
+        return res.status(404).redirect("/login");
+      }
+    } else {
+      console.error("Wrong e-mail or password!!");
+      return res.status(404).redirect("/login");
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).redirect("/login");
+  }
+});
 
-
-router.post('/delete', (req, res) => {
-    User.findOneAndDelete({id: req.body._id }).then(
-        res.render('welcome')
-    ).catch((error) => {
-        res.status(400).json({
-            error: error
-        });
-    })
+router.post("/delete", (req, res) => {
+  User.findOneAndDelete({ id: req.body._id })
+    .then(res.render("welcome"))
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
+      });
+    });
 });
 
 module.exports = router;
