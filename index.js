c3fa9a133d4bf4522805d7fab5f13279d988a572
PipHarsveld <pip.harsveld@outlook.com>
2022-04-01 14:01:06 +0200

    :pencil2: Change name images

--- index.js
+++ index.js
@@ -77,13 +77,9 @@ app.get('/home', async (req, res) => {
 
 app.post("/like", async (req, res) => {
   try {
-    // await restaurant
-    //   .findOneAndUpdate({ name: "The Breakfast Club" }, {$push: {likedby: ["mij"]}})
-    //   .lean()
-    //   .exec();
-      await restaurant.updateOne(
-        { name: "The Breakfast Club" },
-        { $push: { likedby : [ "Mij"] } });
+    await restaurant.updateOne(
+      { name: "The Breakfast Club" },
+      { $push: { likedby : {user: req.body._id} } });
     const data = await restaurant.findOne({ preference: "" }).lean().exec();
     res.render("home", { data: data });
   } catch {
