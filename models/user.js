f547cd1d6e116e27b00762bc4962138aeecd0f4f
PipHarsveld <pip.harsveld@outlook.com>
2022-04-02 17:36:15 +0200

    :construction: Try to update the user db on like

--- models/user.js
+++ models/user.js
@@ -21,7 +21,18 @@ const userSchema = new mongoose.Schema({
     password:{
         type: String,
         required: true
+    },
+    preferences:{
+        id1: {type: String, required: true},
+        id2: {type: String, required: true},
+        id3: {type: String, required: true},
+        id4: {type: String, required: true},
+        id5: {type: String, required: true},
+        id6: {type: String, required: true},
+        id7: {type: String, required: true},
+        id8: {type: String, required: true},
+        id9: {type: String, required: true},
+        id10: {type: String, required: true},
     }
 })
-
 module.exports = mongoose.model("User", userSchema)
