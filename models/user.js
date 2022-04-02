10d9aa1e63f61ee1a0aa1d65587ff057d966b7c0
PipHarsveld <pip.harsveld@outlook.com>
2022-04-02 17:43:19 +0200

    :card_file_box: Update user schema

--- models/user.js
+++ models/user.js
@@ -20,16 +20,16 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     preferences:{
-        id1: {type: String, required: true},
-        id2: {type: String, required: true},
-        id3: {type: String, required: true},
-        id4: {type: String, required: true},
-        id5: {type: String, required: true},
-        id6: {type: String, required: true},
-        id7: {type: String, required: true},
-        id8: {type: String, required: true},
-        id9: {type: String, required: true},
-        id10: {type: String, required: true},
+        id1: {type: String},
+        id2: {type: String},
+        id3: {type: String},
+        id4: {type: String},
+        id5: {type: String},
+        id6: {type: String},
+        id7: {type: String},
+        id8: {type: String},
+        id9: {type: String},
+        id10: {type: String},
     }
 })
 module.exports = mongoose.model("User", userSchema)
