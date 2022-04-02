0a0b6628e11ae40566b3dc62982ecec7fd6167df
Eva Boogaard <evaboogaard@hotmail.com>
2022-04-02 13:19:09 +0200

    :white_check_mark: progressive enhancement for the starting animation

--- static/javascript/welcome.js
+++ static/javascript/welcome.js
@@ -2,6 +2,16 @@ const welcomeLogo = document.querySelector(`.welcome img`);
 const welcomeButton = document.querySelector(`.welcome button`);
 const gettingStarted = document.querySelector(`.gettingstarted`);
 
+// PE: if JS is on, the transition still works but it's not clickable
+const ifJavascript = () => {
+	welcomeLogo.classList.remove(`delay`, `hide`);
+  welcomeButton.classList.remove(`delay`, `hide`);
+  gettingStarted.classList.remove(`show`, `opacitydelay`);
+}
+ifJavascript();
+
+// now it is clickable
+
 welcomeButton.addEventListener(`click`, () => {
   welcomeLogo.classList.add(`hide`);
   welcomeButton.classList.add(`hide`);
