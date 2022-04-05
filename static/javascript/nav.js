3d88298d906872d7b3431a02ce4e58f4b47a0015
Eva Boogaard <evaboogaard@hotmail.com>
2022-04-05 14:20:09 +0200

    :construction: trying to make the nav dynamic

---
+++ static/javascript/nav.js
@@ -0,0 +1,23 @@
+const homeButton = document.querySelector("nav ul li:nth-of-type(1)");
+const likeButton = document.querySelector("nav ul li:nth-of-type(2)");
+const accountButton = document.querySelector("nav ul li:nth-of-type(3)");
+
+homeButton.addEventListener("click", () => {
+    homeButton.classList.add("active");
+    likeButton.classList.remove("active");
+    accountButton.classList.remove("active");
+})
+
+likeButton.addEventListener("click", () => {
+    homeButton.classList.remove("active");
+    likeButton.classList.add("active");
+    accountButton.classList.remove("active");
+})
+
+accountButton.addEventListener("click", () => {
+    homeButton.classList.remove("active");
+    likeButton.classList.remove("active");
+    accountButton.classList.add("active");
+})
+
+console.log("you kiddin g rigte");
